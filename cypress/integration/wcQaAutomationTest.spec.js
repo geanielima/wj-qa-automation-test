9a23b90769a45c4888f1d4fb23c04a6f4708cdaa
geane <geanielima@gmail.com>
2022-02-27 09:41:34 -0300

    adicionando a biblioteca fake.js ao projeto

--- cypress/integration/wcQaAutomationTest.spec.js
+++ cypress/integration/wcQaAutomationTest.spec.js
@@ -2,7 +2,8 @@ describe("Testing wcqaAutomation", () => {
     before(() => {
       cy.visit("/");
     });
-    it.only("Clicar nos botões One,Two e Four e validar a não-presença dos mesmos", () => {
+    it("Clicar nos botões One,Two e Four e validar a não-presença dos mesmos", () => {
+      
         cy.get('#btn_one').should('be.visible').click();
         cy.get('#btn_two').should('be.visible').click();
         cy.get('#btn_three').should('be.visible').click();
@@ -12,9 +13,11 @@ describe("Testing wcqaAutomation", () => {
         cy.get('#btn_three').should('not.be.visible');
     })
     it("validações na tela do wcQA ", () => {
+        //gerador de palavras randomicas 
+        const randomWord = cy.faker.lorem.words();
         //captura o elemento yourFirst name e verifica se está visivel para o usuárioe logo após adiciona o nome;
-        cy.get('#first_name').should("be.visible").type('geane');
-        
+        cy.get('#first_name').should("be.visible").type(randomWord);
+
         //clica no botão one e verifica se está marcado.
         cy.get('#opt_one').check().should('be.checked');
 
